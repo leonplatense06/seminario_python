b5f14a4ae4d695f3cca63f9281ae5d6374c45bca
Ramiro Lavie <lavieramiro@gmail.com>
2025-03-18 14:51:16 -0300

    3ra Modificacion

--- Practica_1/questions.py
+++ Practica_1/questions.py
@@ -24,14 +24,15 @@ answers = [
  # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
+ #formo lista de tuplas, cada tupla contiene una pregunta, sus opciones, y su respuesta correcta, es decir, las agrupe
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+
 puntaje = 0.0
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
- # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions)-1)
+for quest, options, correct_ans in questions_to_ask:
  # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(quest)
+    for i, answer in enumerate(options):
         print(f"{i + 1}. {answer}")
  # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -45,7 +46,7 @@ for _ in range(3):
  # Se verifica si la respuesta del usuario es "posible"
             if (user_answer >= 0) & (user_answer < 4): 
  # Se verifica si la respuesta es correcta
-                if user_answer == correct_answers_index[question_index]:
+                if user_answer == correct_ans:
                     print("¡Correcto!")
                     puntaje += 1.0
                     break
@@ -54,7 +55,7 @@ for _ in range(3):
  # se muestra la respuesta correcta
                     puntaje -= 0.5
                     print("Incorrecto. La respuesta correcta es:")
-                    print(answers[question_index][correct_answers_index[question_index]])
+                    print(options[correct_ans])
  # Si la respesta del usuario no es "posible"
             else:
                 print("Respuesta no válida.")
